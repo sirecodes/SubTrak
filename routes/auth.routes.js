4638cf76198fed5a0ebf1f367744f80bdfb55919
dev-sire <shahidamaan2019@gmail.com>
2025-02-15 23:43:45 +0500

    Finished jwt auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { signOut, signUp, signIn } from "../controllers/auth.controller.js";
 
 const authRouter = Router()
 
-authRouter.post('/sign-up', (req, res) => res.send({ title: 'sign-up' }));
-authRouter.post('/sign-in', (req, res) => res.send({ title: 'sign-in' }));
-authRouter.post('/sign-out', (req, res) => res.send({ title: 'sign-out' }));
+// path: /api/v1/auth/sign-up || sign-in || sign-out
+authRouter.post('/sign-up', signUp);
+authRouter.post('/sign-in', signIn);
+authRouter.post('/sign-out', signOut);
 
 export default authRouter;
