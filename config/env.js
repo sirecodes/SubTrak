37a24b0d3f92989135f737366a27cd75d12e8012
dev-sire <shahidamaan2019@gmail.com>
2025-02-17 22:01:36 +0500

    finalized app

--- config/env.js
+++ config/env.js
@@ -4,10 +4,16 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local`})
 
 export const { 
     PORT, 
-    NODE_ENV, 
+    NODE_ENV,
+    SERVER_URL,
     DB_URI, 
     JWT_SECRET, 
     JWT_EXPIRES_IN, 
     ARCJET_ENV,
-    ARCJET_KEY, 
+    ARCJET_KEY,
+    QSTASH_URL,
+    QSTASH_TOKEN,
+    QSTASH_CURRENT_SIGNING_KEY,
+    STASH_NEXT_SIGNING_KEY,
+    EMAIL_PASSWORD
 } = process.env;
